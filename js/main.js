cf7d5f46756091148d4c43c109a7e6d6c8b7f33f
Kyle Demmerle <kdemm1@umbc.edu>
2016-01-20 15:58:49 -0500

    This commit is meant to simply add misc. empty files to the repository so that I can become familiar with the Commit process

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/*    
+    Author: Kyle Demmerle
+    Date: 01/20/2016
+    Project: Semi-Autonomous IV Pump
+    File: main.js
+    Description: This file will hold the majority of the javascript code for
+                 the WebApp
+*/
