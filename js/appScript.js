e31a654afb1e493ad39000b5ad6e190cd2a59363
Kyle <kdemm1@umbc.edu>
2016-02-05 18:46:05 -0500

    Added three graphs using Smoothie that currently display randomly generated variables. From here will continue working with Smoothie to display wave form data from a file. Also looking into QMQTT pub/sub status for diagnostic data and pump controls

---
+++ js/appScript.js
@@ -0,0 +1,34 @@
+/*
+  Author: Kyle Demmerle
+  Date: 01/22/16
+  Project: Semi-Autonomous IV Pump
+  File: sub.js
+  Description: This file will be used to establish a connection between an MQTT
+  broker and the WebApp. The main way of completeing this is through using Web
+  Sockets that allow us to instantly connect and start receiving data. At some 
+  there will also be a need for creating a publishing application that will
+  publish patient vitals to different sockets, thus allowing for multiple 
+  devices to connect to the server and make use of it
+*/
+
+var line1 = new TimeSeries();
+var line2 = new TimeSeries();
+var line3 = new TimeSeries();
+setInterval(function(){
+    line1.append(new Date().getTime(), Math.random());
+    line2.append(new Date().getTime(), Math.random());
+    line3.append(new Date().getTime(), Math.random());
+}, 1000);
+
+function createNewDiagnostic(){
+    var diag1 = new SmoothieChart();
+    var diag2 = new SmoothieChart();
+    var diag3 = new SmoothieChart();
+    diag1.addTimeSeries(line1);
+    diag2.addTimeSeries(line2);
+    diag3.addTimeSeries(line3);
+    diag1.streamTo(document.getElementById("diag1"), 1000);
+    diag2.streamTo(document.getElementById("diag2"), 1000);
+    diag3.streamTo(document.getElementById("diag3"), 1000);
+}
+
